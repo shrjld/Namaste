b90906db0abfe2cd0026f79719bc405a6738c4eb
SaharJlvnd <shrjld754@gmail.com>
2026-07-16 00:23:35 +0330

    ep1,2,3

---
+++ java script/main.js
@@ -0,0 +1,76 @@
+//namaste EP1,Ep2
+////////////////////////////////////////
+//ex1
+let num=3;
+
+function myFun(myvalue){
+    let sum=myvalue+2;
+    return sum
+
+}
+let result=myFun(num);
+console.log(result);
+//////////////////////////////////////////
+
+//ex2
+
+let a=10;
+function Func(number){
+    let minus=number-10;
+    return minus
+}
+let answer1=Func(a);
+console.log(answer1);
+
+let answer2=Func(20);
+console.log(answer2);
+////////////////////////////////////////////
+//EP3
+
+
+
+var x =2;
+
+function myName(){
+    console.log("Sahar Jalilvand");
+}
+myName();
+console.log(x);
+
+///////////////////////////////////////////////
+
+typey();
+console.log(y);
+
+var y = true;
+
+function typey(){
+    console.log(typeof y);
+}
+////////////////////////////////////////////////
+//arrow function
+
+var language1 = () =>{
+    console.log("java script")
+}
+language1();
+
+console.log(typeof typey);
+console.log(typeof language1);
+
+/////////////////////////////////////////////////
+// language2();
+
+var language2 = () =>{
+    console.log("python");
+}
+
+///////////////////////////////////////////////////
+// another way to write function:
+
+//language3();
+
+var language3 = function() {
+        console.log("c")
+
+}
