dc68bcfd4250b3efaaa84da623cefeeb71aae6f4
SaharJlvnd <shrjld754@gmail.com>
2026-07-19 03:18:05 +0330

    ep 6 & 7

--- java script/main.js
+++ java script/main.js
@@ -1,37 +1,37 @@
 //namaste EP1,Ep2
 ////////////////////////////////////////
 //ex1
-let num=3;
+let num = 3;
 
-function myFun(myvalue){
-    let sum=myvalue+2;
+function myFun(myvalue) {
+    let sum = myvalue + 2;
     return sum
 
 }
-let result=myFun(num);
+let result = myFun(num);
 console.log(result);
 //////////////////////////////////////////
 
 //ex2
 
-let a=10;
-function Func(number){
-    let minus=number-10;
+let a = 10;
+function Func(number) {
+    let minus = number - 10;
     return minus
 }
-let answer1=Func(a);
+let answer1 = Func(a);
 console.log(answer1);
 
-let answer2=Func(20);
+let answer2 = Func(20);
 console.log(answer2);
 ////////////////////////////////////////////
 //EP3
 
 
 
-var x =2;
+var x = 2;
 
-function myName(){
+function myName() {
     console.log("Sahar Jalilvand");
 }
 myName();
@@ -44,13 +44,13 @@ console.log(y);
 
 var y = true;
 
-function typey(){
+function typey() {
     console.log(typeof y);
 }
 ////////////////////////////////////////////////
 //arrow function
 
-var language1 = () =>{
+var language1 = () => {
     console.log("java script")
 }
 language1();
@@ -61,7 +61,7 @@ console.log(typeof language1);
 /////////////////////////////////////////////////
 // language2();
 
-var language2 = () =>{
+var language2 = () => {
     console.log("python");
 }
 
@@ -70,41 +70,41 @@ var language2 = () =>{
 
 //language3();
 
-var language3 = function() {
-        console.log("c")
+var language3 = function () {
+    console.log("c")
 
 }
 //////////////////////////////////////////////////
 //EP4
 
-var z = 2 ;
+var z = 2;
 
 func1();
 func2();
 
 console.log(z);
 
-function func1(){
-    var z = 4 ;
+function func1() {
+    var z = 4;
     console.log(z);
 }
 
-function func2(){
-    var z = 6 ;
+function func2() {
+    var z = 6;
     console.log(z);
 }
 ///////////////////////////////////////////////////////
-function name1(){
-    var firstName = "sahar" ;
+function name1() {
+    var firstName = "sahar";
     console.log(firstName);
 }
 name2();
 name1();
 
-var firstName="zahra";
+var firstName = "zahra";
 
-function name2(){
-    var firstName = "hana" ;
+function name2() {
+    var firstName = "hana";
     console.log(firstName);
 }
 
@@ -113,14 +113,81 @@ console.log(firstName);
 
 //////////////////////////////////////////////////////
 //EP5
-let myLastName="jalilvand";
-let myFirstName="Sahar";
+let myLastName = "jalilvand";
+let myFirstName = "Sahar";
 console.log(myLastName);
 console.log(myFirstName);
 
-var age = () =>{
-    number=50;
+var age = () => {
+    number = 50;
     console.log(number);
 
 };
 age()
+
+//////////////////////////////////////////////////////
+
+//EP 6
+console.log(age1);
+var age1 = 100;
+console.log(age1);
+// console.log(age2);
+
+var age3;
+console.log(age3);
+
+
+//////////////////////////////////////////////////////
+
+//EP 7
+
+
+function funcA() {
+    // funcA();
+    var test="D access every thing"
+    var testA = "C";
+
+    function funcB() {
+        console.log(testA);
+        var testB = "JAVA SCRIPT";
+
+        function funcC() {
+            console.log(testB);
+            var testC = "PYTHON";
+
+            function funcD() {
+                console.log(testC);
+                console.log(test)
+
+            } funcD();
+
+        } funcC();
+
+    } funcB();
+
+}
+
+var testScope = 90;
+funcA();
+/////////////////////////////////
+
+function testNamasteEx1() {
+    var namasteVar = 10;
+    var namasteVar2 = 20;
+
+    console.log(namasteVar2);
+
+    testNamasteEx2()
+    function testNamasteEx2() {
+        console.log(namasteVar);
+
+    }
+
+}
+// console.log(namasteVar);
+// testNamasteEx2();
+testNamasteEx1();
+
+
+
+
